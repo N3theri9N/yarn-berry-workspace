29d91e1dedda118c91ad5eae766b90006bfff1e8
N3theri9N <nnea5215@gmail.com>
2023-05-10 19:51:37 +0900

    Revert "push deploy"
    This reverts commit e491543a45fcf87446615ebfd129982767f330cc.

--- apps/admin/src/pages/index.tsx
+++ apps/admin/src/pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By
+              By{' '}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
